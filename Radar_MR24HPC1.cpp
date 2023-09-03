43410f2d67246c9ae6b7b997eac83b80cacecbfb
Tauno Erik <sedumacre@gmail.com>
2023-09-03 19:14:57 +0300

    renaming

--- Radar_MR24HPC1.cpp
+++ Radar_MR24HPC1.cpp
@@ -1,33 +1,37 @@
+/*
+Copyright 2023 Tauno Erik
+*/
+
 #include "Arduino.h"
 #include "Radar_MR24HPC1.h"
 
 Radar_MR24HPC1::Radar_MR24HPC1(Stream *s)
-    : stream(s){
-  this->is_new_data = false;
+    : stream(s) {
+  this->is_new_frame = false;
 }
 
 /*
-  Receive radar data and store it in data array
+  Receive radar frame and store it in frame array
 */ 
 void Radar_MR24HPC1::read() {
-  unsigned char buffer[DATA_SIZE] = {0};
+  unsigned char buffer[FRAME_SIZE] = {0};
   uint8_t buff_len = 0;
 
   // save to buffer
   while (stream->available()) {
-    // Data start bytes
-    if(stream->read() == HEAD1) {
-      if(stream->read() == HEAD2) {
+    // Frame start bytes
+    if (stream->read() == HEAD1) {
+      if (stream->read() == HEAD2) {
         // Read data
         // What if data is 0x43?? then its stops!!
-        buff_len = stream->readBytesUntil(END2, buffer, DATA_SIZE);
-        //buff_len = stream->readBytes(buffer, DATA_SIZE);
+        buff_len = stream->readBytesUntil(END2, buffer, FRAME_SIZE);
+        // buff_len = stream->readBytes(buffer, DATA_SIZE);
         // Kontrollida kas viimane element on õige:
-        //if (buffer[buff_len-1] != END1) {
+        // if (buffer[buff_len-1] != END1) {
         //  Serial.print(" !! False end !! ");
         //}
-        if (buff_len > 0 && buff_len < DATA_SIZE){
-          buffer[buff_len] = END2; // add last end byte
+        if (buff_len > 0 && buff_len < FRAME_SIZE) {
+          buffer[buff_len] = END2;  // add last end byte
         }
       }
     }
@@ -35,20 +39,20 @@ void Radar_MR24HPC1::read() {
 
   // Save to data_frame
   // Add headers
-  data[0] = HEAD1;
-  data[1] = HEAD2;
+  frame[0] = HEAD1;
+  frame[1] = HEAD2;
 
-  for (int i = 0; i < buff_len+1; i++) {// +1 end2 byte
-    data[i+2] = buffer[i];
+  for (int i = 0; i < buff_len+1; i++) {  // +1 end2 byte
+    frame[i+2] = buffer[i];
   }
 
-  if (is_data_good(data)) {
-    //Serial.print("Good: ");
-    data_len = buff_len + 3;
-    is_new_data = true;
-  }
-  else {
-    //Serial.println(" Bad: ");
+  // Checksum
+  if (is_frame_good(frame)) {
+    // Serial.print("Good: ");
+    frame_len = buff_len + 3;
+    is_new_frame = true;
+  } else {
+  // Serial.println(" Bad: ");
   }
 
 }
@@ -60,22 +64,21 @@ mode: HEX, DEC, CHAR
 Adds headers
 */
 void Radar_MR24HPC1::print(int mode) {
-  if(is_new_data) {
-
+  if (is_new_frame) {
     switch (mode) {
       case DEC:
         Serial.print(' ');
-        print_dec(data, data_len);
+        print_dec(frame, frame_len);
         break;
         break;
-      default: // HEX
+      default:  // HEX
         Serial.print(' ');
-        print_hex(data, data_len);
+        print_hex(frame, frame_len);
         break;
     }
 
-    is_new_data = false;
-    data[data_len] = {0};  // set all data to zero
+    is_new_frame = false;
+    frame[frame_len] = {0};  // set all data to zero
   }
 }
 
@@ -88,12 +91,12 @@ void Radar_MR24HPC1::print(int mode) {
 void Radar_MR24HPC1::print_hex(const unsigned char* buff, int len) {
   char charVal[4];
 
-  for(int i=0; i<len; i++){
+  for (int i=0; i < len; i++) {
     sprintf(charVal, "%02X", buff[i]);
     Serial.print(charVal);
     Serial.print(' ');
   }
-  
+
   Serial.println();
 }
 
@@ -105,12 +108,12 @@ void Radar_MR24HPC1::print_hex(const unsigned char* buff, int len) {
 void Radar_MR24HPC1::print_dec(const unsigned char* buff, int len) {
   char char_val[4];
 
-  for(int i=0; i<len; i++){
+  for (int i=0; i < len; i++) {
     sprintf(char_val, "%d", buff[i]);
     Serial.print(char_val);
     Serial.print(' ');
   }
-  
+
   Serial.println();
 }
 
@@ -118,95 +121,95 @@ void Radar_MR24HPC1::print_dec(const unsigned char* buff, int len) {
 
 */
 void Radar_MR24HPC1::analys(bool show_bodysign) {
-  read();
+  read();  // ?
 
-  if (is_new_data) {
+  if (is_new_frame) {
     // Read control word
-    int control_word = data[I_CONTROL_WORD];
+    int control_word = frame[I_CONTROL_WORD];
 
-    //print();
+    // print();
 
     if (control_word == HUMAN_STATUS) {
-      int report = data[I_CMD_WORD];  // Read command word
+      int report = frame[I_CMD_WORD];  // Read command word
 
       if (report == PRESENCE_REPORT) {
         // Read data byte
-        int d = data[I_DATA];  // OCCUPIED, UNOCCUPIED
+        int d = frame[I_DATA];  // OCCUPIED, UNOCCUPIED
         if (d == 0) {
           status_msg = NOONE;
-        }
-        else { // == 1
+        } else {  // == 1
           status_msg = SOMEONE;
         }
-      }
-      else if (report == MOTION_REPORT) {
-        int d = data[I_DATA];  // NONE, MOTIONLESS, ACTIVE
+      } else if (report == MOTION_REPORT) {
+        int d = frame[I_DATA];  // NONE, MOTIONLESS, ACTIVE
         switch (d) {
           case NONE:
-            status_msg = NOTHING; break;
+            status_msg = NOTHING;
+            break;
           case MOTIONLESS:
-            status_msg = SOMEONE_STOP; break;
+            status_msg = SOMEONE_STOP;
+            break;
           case ACTIVE:
-            status_msg = SOMEONE_MOVE; break;
+            status_msg = SOMEONE_MOVE;
+            break;
         }
-      }
-      else if (report == MOVMENT_PARAM) {
+      } else if (report == MOVMENT_PARAM) {
         status_msg = HUMANPARA;
-        bodysign_val = data[I_DATA];
-      }
-      else if (report == PROX_REPORT) {
-        int d = data[I_DATA]; // NONE, NEAR, FAR
+        bodysign_val = frame[I_DATA];
+      } else if (report == PROX_REPORT) {
+        int d = frame[I_DATA];  // NONE, NEAR, FAR
         switch (d) {
           case NONE:
-            status_msg = NOTHING; break;
+            status_msg = NOTHING;
+            break;
           case NEAR:
-            status_msg = SOMEONE_CLOSE; break;
+            status_msg = SOMEONE_CLOSE;
+            break;
           case FAR:
-            status_msg = SOMEONE_AWAY; break;
+            status_msg = SOMEONE_AWAY;
+            break;
         }
       }
-    }
-    else if (control_word == DETAIL_STATUS) {
-      int report = data[I_CMD_WORD];  // Read command word
+    } else if (control_word == DETAIL_STATUS) {
+      int report = frame[I_CMD_WORD];  // Read command word
 
       if (report == SENSOR_REPORT) {
-        //int d = data[4];
+        // int d = data[4];
         status_msg  = DETAILMESSAGE;
 
-        static_energy  = data[I_DATA];
-        static_dist = data[I_DATA + 1];
-        motion_energy = calculate_distance(data[I_DATA + 2]);
-        motion_dist  = calculate_distance(data[I_DATA + 3]);
-        motion_speed = calculate_speed(data[I_DATA + 4]);
-      }
-      else if (report == DET_PROX_REPORT) {
-        int d = data[I_DATA]; // NONE, NEAR, FAR
-        switch(d) {
+        static_energy  = frame[I_DATA];
+        static_dist = frame[I_DATA + 1];
+        motion_energy = calculate_distance(frame[I_DATA + 2]);
+        motion_dist  = calculate_distance(frame[I_DATA + 3]);
+        motion_speed = calculate_speed(frame[I_DATA + 4]);
+      } else if (report == DET_PROX_REPORT) {
+        int d = frame[I_DATA];  // NONE, NEAR, FAR
+        switch (d) {
           case NONE:
-            status_msg = NOTHING; break;
+            status_msg = NOTHING;
+            break;
           case NEAR:
-            status_msg = SOMEONE_CLOSE; break;
+            status_msg = SOMEONE_CLOSE;
+            break;
           case FAR:
-            status_msg = SOMEONE_AWAY; break;
+            status_msg = SOMEONE_AWAY;
+            break;
         }
-      }
-      else if (report == DET_MOVMENT_PARA) {
+      } else if (report == DET_MOVMENT_PARA) {
         status_msg = HUMANPARA;
-        bodysign_val = data[I_DATA];
+        bodysign_val = frame[I_DATA];
       }
     }
-
   }
-  is_new_data = false;
+  is_new_frame = false;
 }
 
 
 
-//Send data frame
-void Radar_MR24HPC1::write_cmd(const unsigned char* buff, int len, bool cyclic /*=false*/) {
-
-  if (cyclic || count < checkdata_len) {
-
+// Send data frame
+void Radar_MR24HPC1::write_cmd(const unsigned char* buff,
+                              int len, bool cyclic) {
+  if (cyclic || count < checkframe_len) {
     if (cyclic || count < 1) {
       stream->write(buff, len);
       stream->flush();
@@ -216,7 +219,7 @@ void Radar_MR24HPC1::write_cmd(const unsigned char* buff, int len, bool cyclic /
     do {
       read();
       delay(20);
-    } while (!(this->is_new_data));
+    } while (!(this->is_new_frame));
 
     if (cyclic || count < 1) {
       Serial.print("  Sent  ---> ");
@@ -228,7 +231,7 @@ void Radar_MR24HPC1::write_cmd(const unsigned char* buff, int len, bool cyclic /
       print();
     }
 
-    is_new_data = false;
+    is_new_frame = false;
   }
   count++;
 }
@@ -237,33 +240,39 @@ void Radar_MR24HPC1::write_cmd(const unsigned char* buff, int len, bool cyclic /
 Return Radar ID
 */
 int Radar_MR24HPC1::get_id() {
+  const unsigned char id_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x02, 0xA2, 0x00, 0x01, 0x0F, 0x60, END1, END2};
+
   stream->write(id_cmd, CMD_LEN);
   stream->flush();
 
   // read radar responce
   do {
     read();
-    //delay(10);
-  } while (!(is_new_data));
+    // delay(10);
+  } while (!(is_new_frame));
 
   print(DEC);
-  is_new_data = false;
+  is_new_frame = false;
   // TODO: return id
   return 0;
 }
 
 int Radar_MR24HPC1::firm_ver_id() {
+  const unsigned char firmware_ver_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x02, 0xA4, 0x00, 0x01, 0x0F, 0x62, END1, END2};
+
   stream->write(firmware_ver_cmd, CMD_LEN);
   stream->flush();
 
   // read radar responce
   do {
     read();
-    //delay(10);
-  } while (!(is_new_data));
+    // delay(10);
+  } while (!(is_new_frame));
 
   print();
-  is_new_data = false;
+  is_new_frame = false;
   // TODO: return id
   return 0;
 }
@@ -273,9 +282,12 @@ Reset radar
 returns 1
 */
 int Radar_MR24HPC1::reset() {
+  const unsigned char reset_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x01, 0x02, 0x00, 0x01, 0x0F, 0xBF, END1, END2};
+
   stream->write(reset_cmd, CMD_LEN);
   stream->flush();
-  //Serial.println("Radar reset!");
+  // Serial.println("Radar reset!");
   return 1;
 }
 
@@ -287,12 +299,10 @@ float Radar_MR24HPC1::calculate_distance(int val) {
 float Radar_MR24HPC1::calculate_speed(int val) {
   if (val == 0x0A) {
     return 0;
-  }
-  else if (val > 0x0A) {
+  } else if (val > 0x0A) {
     // Negative speed
     return -((val-10)*UNIT);
-  }
-  else if (val < 0x0A) {
+  } else if (val < 0x0A) {
     // Positive speed
     return (val)*UNIT;
   }
@@ -305,7 +315,7 @@ float Radar_MR24HPC1::calculate_speed(int val) {
 */
 int Radar_MR24HPC1::hex_to_int(const unsigned char *hexChar) {
   // Use strtol to convert the hexadecimal string to an integer
-  //return strtol(hexChar, NULL, 16);
+  // return strtol(hexChar, NULL, 16);
   int intValue = 0;
 
     if (*hexChar >= '0' && *hexChar <= '9') {
@@ -328,7 +338,7 @@ int Radar_MR24HPC1::hex_to_int(const unsigned char *hexChar) {
 char Radar_MR24HPC1::hex_to_char(const unsigned char *hex) {
   int int_val = hex_to_int(hex);
   // Cast the integer value to a char
-  char char_val = (char)int_val;
+  char char_val = static_cast <char> (int_val);  // (char)int_val;
 
   return char_val;
 }
@@ -336,11 +346,11 @@ char Radar_MR24HPC1::hex_to_char(const unsigned char *hex) {
 /*
  Calcutaltes checksum
 */
-uint8_t Radar_MR24HPC1::calculate_sum(const unsigned char data[], int size) {
+uint8_t Radar_MR24HPC1::calculate_sum(const unsigned char f[], int size) {
   uint8_t sum = 0;
 
   for (int i = 0; i < size; i++) {
-    sum += data[i];
+    sum += f[i];
   }
 
   return sum;
@@ -349,18 +359,134 @@ uint8_t Radar_MR24HPC1::calculate_sum(const unsigned char data[], int size) {
 /*
  Compare data sum and calculated sum
 */
-bool Radar_MR24HPC1::is_data_good(const unsigned char data[]) {
-  unsigned char data_lenght_byte = data[I_LENGHT_L];
+bool Radar_MR24HPC1::is_frame_good(const unsigned char f[]) {
+  unsigned char data_lenght_byte = f[I_LENGHT_L];
 
-  int count = I_DATA + data_lenght_byte; // how many bytes total
+  int count = I_DATA + data_lenght_byte;  // how many bytes total
 
-  uint8_t my_sum = calculate_sum(data, count);
+  uint8_t my_sum = calculate_sum(f, count);
 
-  uint8_t data_sum = data[I_DATA + data_lenght_byte];
+  uint8_t frame_sum = f[I_DATA + data_lenght_byte];
 
-  if (data_sum == my_sum) {
+  if (frame_sum == my_sum) {
     return true;
   }
 
   return false;
+}
+
+/*
+Turn ON Underlying Open function
+*/
+int Radar_MR24HPC1::underlying_on() {
+  const unsigned char on_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x08, 0x00, 0x00, 0x01, 0x01, 0xB6, END1, END2};
+
+  stream->write(on_cmd, CMD_LEN);
+  stream->flush();
+
+  return 1;
+}
+
+/*
+Turn OFF Underlying Open function
+*/
+int Radar_MR24HPC1::underlying_off() {
+  const unsigned char off_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x08, 0x00, 0x00, 0x01, 0x00, 0xB5, END1, END2};
+
+  stream->write(off_cmd, CMD_LEN);
+  stream->flush();
+
+  return 1;
+}
+
+/*
+Returns Underlying Open function status:
+1-ON
+0-OFF
+*/
+int Radar_MR24HPC1::underlying_status() {
+  const unsigned char status_cmd[CMD_LEN] = {
+    HEAD1, HEAD2, 0x08, 0x80, 0x00, 0x01, 0x0F, 0x44, END1, END2};
+
+  // Send command
+  stream->write(status_cmd, CMD_LEN);
+  stream->flush();
+  // Read response
+  do {
+    read();
+    // delay(10);
+  } while (!(is_new_frame));
+
+  Serial.print("Response: ");
+  print();
+  is_new_frame = false;
+  return 0;
+}
+
+/*
+Translate the responses into human-readable form
+*/
+void Radar_MR24HPC1::translate() {
+  if (is_new_frame) {
+    int control_word = frame[I_CONTROL_WORD];
+
+    int cmd_word = frame[I_CMD_WORD];
+    // int data = frame[I_DATA];
+    int len = frame[I_LENGHT_L];
+
+    switch (control_word) {
+      case 0x01:
+        translate_01();
+        break;
+      case 0x02:
+        translate_02(cmd_word, len);
+        break;
+      default:
+        print();
+        break;
+    }
+  }
+}
+
+/*
+translate() helper func
+Controll word 0x01
+Hearbeat, reset
+*/
+void Radar_MR24HPC1::translate_01() {
+  int cmd_word = frame[I_CMD_WORD];
+  int data = frame[I_DATA];
+
+  if (cmd_word == 0x01 && data == 0x0F) {
+    Serial.println("Heartbeat");
+  } else if (cmd_word == 0x02 && data == 0x0F) {
+    Serial.println("Reset");
+  }
+}
+
+/*
+translate() helper func
+Controll word 0x02
+Product info
+*/
+void Radar_MR24HPC1::translate_02(const unsigned char cmd_word,
+                                  int len) {
+  // Product Model
+  if (cmd_word == 0xA1) {
+    Serial.print("Product Model");
+  }
+  // Product ID
+  if (cmd_word == 0xA2) {
+    Serial.print("Product ID");
+  }
+  // Hardware Model
+  if (cmd_word == 0xA3) {
+    Serial.print("Hardware Model");
+  }
+  // Firmware version
+  if (cmd_word == 0xA3) {
+    Serial.print("Firmware version");
+  }
 }
--- Radar_MR24HPC1.h
+++ Radar_MR24HPC1.h
@@ -1,5 +1,9 @@
-#ifndef _Radar_MR24HPC1_H__
-#define _Radar_MR24HPC1_H__
+/*
+Copyright 2023 Tauno Erik
+*/
+
+#ifndef LIB_RADAR_MR24HPC1_RADAR_MR24HPC1_H_
+#define LIB_RADAR_MR24HPC1_RADAR_MR24HPC1_H_
 
 // Frame Headers
 #define HEAD1            0x53  // Frame header 1
@@ -21,7 +25,7 @@
 #define MOTION_REPORT    0x02  // Motion report  //HUMAN_MOVE
 #define MOVMENT_PARAM    0x03  // Body movment/Signs Parameters  //HUMAN_SIGN
 #define NO_HUMAN_SET     0x0A  // Time for entering no person state setting
-#define PROX_REPORT      0x0B  // Proximity report //HUMAN_DIRECT 
+#define PROX_REPORT      0x0B  // Proximity report //HUMAN_DIRECT
 
 // Presense reporting data
 #define UNOCCUPIED       0x00
@@ -33,27 +37,27 @@
 #define ACTIVE           0x02
 
 // Proximity reporting data
-#define NEAR             0x01       //Someone approaches //CA_CLOSE
-#define FAR              0x02       //Some people stay away //CA_AWAY
+#define NEAR             0x01       // Someone approaches //CA_CLOSE
+#define FAR              0x02       // Some people stay away //CA_AWAY
 
 // Underlying Open function Command Word
-#define SENSOR_REPORT    0x01       //DETAILINFO
-#define DET_PROX_REPORT  0x06       //Human movement trends //DETAILDIRECT
-#define DET_MOVMENT_PARA 0x07       //Body Signs Parameters //DETAILSIGN
+#define SENSOR_REPORT    0x01       // DETAILINFO
+#define DET_PROX_REPORT  0x06       // Human movement trends //DETAILDIRECT
+#define DET_MOVMENT_PARA 0x07       // Body Signs Parameters //DETAILSIGN
 
 
-//Return statues, Use in arduino
-#define SOMEONE       0x01       //There are people
-#define NOONE         0x02       //No one
-#define NOTHING       0x03       //No message
-#define SOMEONE_STOP  0x04       //Somebody stop
-#define SOMEONE_MOVE  0x05       //Somebody move
-#define HUMANPARA     0x06       //Body Signs Parameters
-#define SOMEONE_CLOSE 0x07       //Someone approaches
-#define SOMEONE_AWAY  0x08       //Some people stay away
-#define DETAILMESSAGE 0x09       //Underlying parameters of the human state
+// Return statues, Use in arduino
+#define SOMEONE       0x01       // There are people
+#define NOONE         0x02       // No one
+#define NOTHING       0x03       // No message
+#define SOMEONE_STOP  0x04       // Somebody stop
+#define SOMEONE_MOVE  0x05       // Somebody move
+#define HUMANPARA     0x06       // Body Signs Parameters
+#define SOMEONE_CLOSE 0x07       // Someone approaches
+#define SOMEONE_AWAY  0x08       // Some people stay away
+#define DETAILMESSAGE 0x09       // Underlying parameters of the human state
 
-#define UNIT          0.5        //Calculate unit steps
+#define UNIT          0.5        // Calculate unit steps
 
 // Data bytes indexes
 #define I_HEAD1        0  // Frame header 1
@@ -64,60 +68,58 @@
 #define I_LENGHT_L     5  // How many bytes of data
 #define I_DATA         6  // Beginning of data bytes
 //
-#define DATA_SIZE     14  // Max data frame size in bytes
+#define FRAME_SIZE     14  // Max data frame size in bytes
 
 // Reset cmd
 #define CMD_LEN 10       // Reset data frame length
-const unsigned char reset_cmd[CMD_LEN] = {
-    HEAD1, HEAD2, 0x01, 0x02, 0x00, 0x01, 0x0F, 0xBF, END1, END2 };
 
-// Get id cmd
-const unsigned char id_cmd[CMD_LEN] = {
-    HEAD1, HEAD2, 0x02, 0xA2, 0x00, 0x01, 0x0F, 0x60, END1, END2 };
-
-const unsigned char firmware_ver_cmd[CMD_LEN] = {
-    HEAD1, HEAD2, 0x02, 0xA4, 0x00, 0x01, 0x0F, 0x62, END1, END2 };
 
 class Radar_MR24HPC1 {
+ private:
+    Stream *stream;  // e.g. SoftwareSerial or Serial1
+    bool is_new_frame;  // is_new_data
+    uint8_t frame_len;  // data_len
 
-    private:
-        Stream *stream;               // e.g. SoftwareSerial or Serial1
-        bool is_new_data;
-        uint8_t data_len;
+    unsigned char frame[FRAME_SIZE] = {0};  // data
+    int count = 0;
+    int checkframe_len = 2;  // Without cyclic sending, number of frames sent
 
-        unsigned char data[DATA_SIZE] = {0};  // data returned by the radar up to a maximum length
-        int count = 0;
-        int checkdata_len = 2;        // Without cyclic sending, number of frames sent
+    float calculate_distance(int val);
+    float calculate_speed(int val);
+    int hex_to_int(const unsigned char *hexChar);
+    char hex_to_char(const unsigned char *hex);
+    void translate_01();
+    void translate_02(const unsigned char word, int len);
 
-        float calculate_distance(int val);
-        float calculate_speed(int val);
-        int hex_to_int(const unsigned char *hexChar);
-        char hex_to_char(const unsigned char *hex);
+ public:
+    int status_msg = 0;     // Status message
+    int bodysign_val = 0;
 
-    public:
-        int status_msg = 0;     // Status message
-        int bodysign_val = 0;
+    int static_energy = 0;     // Spatial static values
+    float static_dist = 0.0;   // Distance to stationary object m
+    int motion_energy = 0;
+    float motion_dist = 0.0;   // Distance from the moving object m
+    float motion_speed = 0.0;  // Speed of moving object m/s
 
-        int static_energy = 0;     // Spatial static values
-        float static_dist = 0.0;   // Distance to stationary object m
-        int motion_energy = 0;
-        float motion_dist = 0.0;   // Distance from the moving object m
-        float motion_speed = 0.0;  // Speed of moving object m/s
+    Radar_MR24HPC1(Stream *s);
 
-        Radar_MR24HPC1(Stream *s);
-
-        void read();
-        void print(int mode = HEX);
-        void analys(bool show_bodysign);
-        void write_cmd(const unsigned char* buff, int len, bool cyclic = false);  // checkSetMode_func
-        int reset();
-        int get_id();
-        int firm_ver_id();
-        void print_hex(const unsigned char* buff, int len);
-        void print_dec(const unsigned char* buff, int len);
-        // Calculate checksum
-        uint8_t calculate_sum(const unsigned char data[], int size);
-        bool is_data_good(const unsigned char data[]);
+    void read();
+    void print(int mode = HEX);
+    void analys(bool show_bodysign);
+    // checkSetMode_func:
+    void write_cmd(const unsigned char* buff, int len, bool cyclic = false);
+    int reset();
+    int get_id();
+    int firm_ver_id();
+    void print_hex(const unsigned char* buff, int len);
+    void print_dec(const unsigned char* buff, int len);
+    // Calculate checksum
+    uint8_t calculate_sum(const unsigned char f[], int size);
+    bool is_frame_good(const unsigned char f[]);  // Private
+    int underlying_on();
+    int underlying_off();
+    int underlying_status();
+    void translate();
 };
 
-#endif
+#endif  // LIB_RADAR_MR24HPC1_RADAR_MR24HPC1_H_
